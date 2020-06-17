7d34e0d327676e8666a0e7878c17ecb7821bbb52
Alexandra Verzier <alexandraverzier@gmail.com>
2020-06-17 21:40:41 +0200

    first commit for the Cadavre Exquis project

---
+++ public/Draggable.js
@@ -0,0 +1,58 @@
+class Draggable {
+  constructor(word, posX, posY, txtSize) {
+    this.word = word;
+    this.posX = posX;
+    this.posY = posY;
+    this.h = txtSize + 10;
+    this.w = 0;
+    this.dragged = false;
+    this.size = txtSize;
+    this.offset_x = 0;
+    this.offset_y = 0;
+
+  }
+
+  displayWords() {
+    textSize(this.size);
+    this.w = textWidth(this.word) + 10;
+    noStroke();
+    fill(255);
+    textAlign(CENTER, BOTTOM);
+    text(this.word, this.posX, this.posY, this.w, this.h);
+    rectMode(CORNER);
+    noFill();
+    stroke(255);
+    rect(this.posX, this.posY, this.w, this.h);
+
+
+  }
+
+  updatePos() {
+    if (this.dragged == true) {
+      this.posX = mouseX - this.offset_x;
+      this.posY = mouseY - this.offset_y;
+
+    }
+  }
+
+  startDrag() {
+    if(mouseX > this.posX && mouseX < this.posX + this.w) {
+     if(mouseY > this.posY && mouseY < this.posY + this.h) {
+      this.offset_x = mouseX - this.posX;
+      this.offset_y = mouseY - this.posY;
+      this.dragged = true;
+
+     }
+   }
+
+  }
+
+  stopDrag() {
+
+    if(this.dragged) {
+    this.dragged = false;
+    return 1;
+  }
+  return 0;
+}
+}
---
+++ public/sketch.js
@@ -0,0 +1,73 @@
+console.log('this is working');
+
+let allMyWords = [];
+
+
+function setup() { 
+
+//position is initalized at random, here in the window width & height
+let canvas_w = windowWidth; 
+let canvas_h = windowHeight;
+
+
+//This happens everytime there is a new input > click on the 'submit' button.
+    document.getElementById("submit").addEventListener("click", async event => {
+      const someX = Math.floor(Math.random() * canvas_w/2); 
+      const someY = Math.floor(Math.random() * canvas_h/2); 
+
+      const wordIn = document.getElementById('wordInput').value;
+   
+      const data = {someX,someY, wordIn}; 
+
+    const options = {
+    //Specify the method
+    method: 'POST',
+    //Specify the type of data sent
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    //Convert JS object to JSON string
+    body: JSON.stringify(data)
+  };
+
+  //send the object 'options' to /api. 
+  const response = await fetch('/api', options);
+  const myInput = await response.json();
+  console.log(myInput);
+
+  const newEntry = document.createElement('p');
+  newEntry.textContent = `user says: ${myInput.data.wordIn}`
+  const position = document.createElement('div');
+  position.textContent = `and is located at: ${myInput.data.someX}, ${myInput.data.someY}`;
+
+  newEntry.append(position);
+  document.getElementById("list").appendChild(newEntry);
+
+  });
+
+  //this function retrieves all the words previously submitted by querying the DB on the server
+  getData();
+
+  //This time i don't need to specify the method bc GET is the default
+  async function getData() {
+    const response_2 = await fetch('/api');
+    const listWord = await response_2.json();
+    console.log(listWord);
+
+    for (item of listWord) {
+    const root = document.createElement('p');
+    const word = document.createElement('div');
+    word.textContent = `user says: ${item.wordIn}`;
+
+    const position = document.createElement('div'); 
+    position.textContent = `and is located at: ${item.someX}, ${item.someY}`;
+
+
+    root.append(word, position);
+    document.getElementById("list_new").appendChild(root);
+
+  }
+  }
+}
+
+
