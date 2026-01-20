3e2768e17ba3a8cf612deff5d8a21ec12916352c
Alexandra Verzier <alexandraverzier@gmail.com>
2026-01-20 21:09:51 +0100

    cleanup

--- exportDB.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-const lines = fs.readFileSync('wordDB.db', 'utf-8').split('\n').filter(Boolean);
-const docs = lines.map(line => JSON.parse(line));
-
-fs.writeFileSync('wordDB.json', JSON.stringify(docs, null, 2));
-console.log('Exported wordDB.json!');
